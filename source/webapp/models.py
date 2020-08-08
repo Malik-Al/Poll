7497063f1cc0d88e71dae41d2e28fea0b4ff85bc
Malik-94 <imenov-a@mail.ru>
2020-08-09 05:56:16 +0600

    Создал модели Poll Choice,приминил миграцию, заполнил данные , создал фикстуру

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200, verbose_name='Опрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создание')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=200, verbose_name='Текст')
+    poll = models.ForeignKey('webapp.Poll', verbose_name='Опрос', on_delete=models.CASCADE, related_name='choices')
+
+    def __str__(self):
+        return self.text
